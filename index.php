e31a6973844109e2b8f37d72fc010fd859495dad
khoursaaf <khoursa@gmail.com>
2018-11-19 10:05:09 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercise</title>
+    </head>
+    <body>
+        <p>Créer une variable name et l'initialiser avec la valeur de votre choix.
+        Afficher : "Bonjour" + name + ", comment vas tu ?".</p>
+        <?php $name = "Khoursa";
+        echo "Bonjour ".$name.", comment vas tu ?"; ?>
+    </body>
+</html>
